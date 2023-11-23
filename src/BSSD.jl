d442df2444e7df448e37d18721adaf061090c3ff
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2023-11-23 21:35:01 +0100

    need to export function, use EM namespace

--- src/BSSD.jl
+++ src/BSSD.jl
@@ -4,9 +4,11 @@ using Turing
 using StatsPlots
 using Distributions
 using Statistics
+using ExpectationMaximization
 
 export 
-    meta_analytic
+    meta_analytic,
+    fit_beta_mixture
 
 include("meta_analytic.jl")
 include("fit_beta_mixture.jl")
